99ccd5df052d071d7b6951f253874e1fccabdd55
Liao Zhu <liao.zhu.cs@gmail.com>
2022-06-14 21:50:53 -0500

    Styling

--- script.js
+++ script.js
@@ -3,5 +3,10 @@ let board = document.querySelector("#board-container");
 for(let i = 0; i < 16; i++) {
     let pixel = document.createElement("div");
     pixel.classList.add("pixel");
+    pixel.addEventListener("click", myFunction);
     board.appendChild(pixel);
+}
+
+function myFunction() {
+    alert("hello world");
 }
