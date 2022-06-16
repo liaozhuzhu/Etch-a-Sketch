1fff1dfe6becbc2cf52a2597b1f3bf290e9b551b
Liao Zhu <liao.zhu.cs@gmail.com>
2022-06-16 13:33:30 -0500

    Working on Color Selector

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 let board = document.querySelector("#board-container");
+let currColor = "#000000";
+let mousePress = false;
 
+// Check Mouse Down or Up
+document.body.onmousedown = function() { 
+    mousePress = true;
+}
+
+document.body.onmouseup = function() {
+    mousePress = false;
+}
+
+// Choose Color 
+function colorPick(color) {
+    console.log("#34ebde");
+    color = "#34ebde";
+}
+// Fill Grid Based on Size 
 function fillGrid(size) {
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`
@@ -13,8 +30,11 @@ function fillGrid(size) {
     }
 }
 
+// Color Each Pixel
 function colorPixel(e) {
-    e.target.style.backgroundColor = "blue";
+    if (e.type == "mouseover" && mousePress) {
+        e.target.style.backgroundColor = currColor;
+    } 
 }
 
 fillGrid(16);
