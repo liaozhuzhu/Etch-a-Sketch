b0a40b323e589c255d6d09bc77594d39cab62812
Liao Zhu <liao.zhu.cs@gmail.com>
2022-06-16 20:25:23 -0500

    Working on Grid Toggle

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 let body = document.body;
 let board = document.querySelector("#board-container");
 
-let currSize = document.getElementById("size-selector").value;
-let sizeSelect = document.getElementById("size-selector");
-let sizeValue = document.getElementById("size-value");
-sizeValue.innerHTML = currSize;
-
 // Paint or Erase
 let paintBtn = document.getElementById("paint");
 let eraseBtn = document.getElementById("erase");
@@ -48,10 +43,30 @@ colorSelect.oninput = function() {
 }
 
 // Choose Size
+let currSize = document.getElementById("size-selector").value;
+let sizeSelect = document.getElementById("size-selector");
+let sizeValue = document.getElementById("size-value");
+sizeValue.innerHTML = currSize;
+
 sizeSelect.oninput = function() {
     sizeValue.innerHTML = this.value;
     currSize = sizeSelect.value;
     reloadGrid();
+}
+
+// Toggle Grid
+let gridBtn = document.getElementById("grid-toggle");
+let pixelId = document.getElementById("pixel-id");
+let outlineOn = false;
+
+function toggleGrid() {
+    if (outlineOn == false) {
+        pixelId.style.border = "1px solid #000000";
+        outlineOn = true;
+    }
+    else {
+        pixelId.style.border = "none";
+    }
 }
 
 // Reload Grid
@@ -68,6 +83,7 @@ function fillGrid(size) {
     for (let i = 0; i < size * size; i++) {
       const pixel = document.createElement("div");
       pixel.classList.add("pixel");
+      pixel.setAttribute("id", "pixel-id");
       pixel.setAttribute("draggable", false);
       pixel.addEventListener("mouseover", colorPixel);
       pixel.addEventListener("mousedown", colorPixel);
@@ -96,7 +112,10 @@ function clearGrid() {
 paintBtn.addEventListener("click", paint);
 eraseBtn.addEventListener("click", erase);
 clearBtn.addEventListener("click", reloadGrid);
+gridBtn.addEventListener("click", toggleGrid);
+
 
 window.onload = () => {
     fillGrid(currSize);
+    toggleGrid(outlineOn);
 }
